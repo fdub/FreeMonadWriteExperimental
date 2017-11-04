8c27e008a728399835de9c094a55277afcd75fa0
fdub <fdub@posteo.de>
2017-11-04 11:13:48 +0100

    Initial commit

---
+++ Actions/Actions.fs
@@ -0,0 +1,28 @@
+﻿namespace FreeActions
+
+open System
+
+type ActionInstruction<'a> = 
+    | Invoke of Action * (unit -> 'a)
+
+type ActionProgram<'a> =
+    | Free of ActionInstruction<ActionProgram<'a>>
+    | Pure of 'a
+
+module Actions =
+    let private mapI f = function
+    | Invoke (x, next) -> Invoke (x, next >> f)
+
+    let rec private bind f = function
+    | Free x -> x |> mapI (bind f) |> Free
+    | Pure x -> f x
+        
+    let private invoke a = Free (Invoke (a, Pure))
+
+    let abort = Pure ()
+
+    let bindInvoke (a, p) = bind (fun () -> invoke a) p
+    
+    let rec interpret = function
+    | Pure x -> x
+    | Free (Invoke (x, next)) -> x.Invoke () |> next |> interpret
---
+++ FreeMonadWrite/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Microsoft.FSharp.Core;
+using FreeActions;
+using Actions = FreeActions.Actions;
+
+namespace FreeMonadWrite
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var value = "initial";
+            var program = Actions.abort;
+            var writer = new Writer(s => value = s);
+            var running = true;
+
+            do
+            {
+                (running, program) = Process(Console.ReadLine(), writer, program);
+            } while (running);
+
+            Actions.interpret(program);
+            Console.WriteLine(value);
+        }
+
+        public static (bool running, ActionProgram<Unit> newProgram) Process(
+            string command,
+            Writer writer,
+            ActionProgram<Unit> program)
+        {
+            if (command.Any())
+            {
+                switch (command[0])
+                {
+                    case 'w':
+                    case 'W':
+                        return (true, Actions.bindInvoke(() => writer.Write(command.Substring(1)), program));
+                    case 'a':
+                    case 'A':
+                        return (false, Actions.abort);
+                }
+            }
+
+            return (false, program);
+        }
+    }
+
+    public class Writer
+    {
+        private readonly Action<string> _commitAction;
+
+        public Writer(Action<string> commitAction)
+        {
+            _commitAction = commitAction;
+        }
+
+        public void Write(string s)
+        {
+            Console.WriteLine($"--> Write({s})");
+            _commitAction(s);
+        }
+    }
+}
